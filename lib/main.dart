99b12ba148dbfc958e799276dcd84a6c4a8a7a14
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2022-05-23 15:33:27 +0530

    Code refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -7,55 +7,91 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const double _size = 100.0;
+  static const double _gap = 25;
+  static const bool _isPlaying = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Color Game',
         home: Scaffold(
             appBar: AppBar(title: const Text('Color Game')),
-            body: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Row(     
-                      mainAxisAlignment: MainAxisAlignment.center, 
-                      crossAxisAlignment: CrossAxisAlignment.center,                
-                      children:const <Widget>[                                        
-                        CircleButton(size:50, color:Colors.red),
-                        VerticalDivider(width: 10,),
-                        CircleButton(size:50.0, color:Colors.yellow),
-                    ]),
-                    Container(
-                      height: 10,
-                    ),
-                    Row(     
-                      mainAxisAlignment: MainAxisAlignment.center, 
-                      crossAxisAlignment: CrossAxisAlignment.center,                
-                      children:const <Widget>[                                        
-                        CircleButton(size:50, color:Colors.blue),
-                        VerticalDivider(width: 10,),
-                        CircleButton(size:50.0, color:Colors.green),
-                    ]),
-                  const Text("Welcome to the color game!",textAlign: TextAlign.center,), 
-                ])));
+            body: HomeScreen(
+              size: _size,
+              gap: _gap,
+            )));
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final double size;
+  final double gap;
+
+  const HomeScreen({super.key, required this.size, required this.gap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleButton(size: size, color: Colors.red),
+                VerticalDivider(
+                  width: gap,
+                ),
+                CircleButton(size: size, color: Colors.yellow),
+              ]),
+          Container(
+            height: gap,
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleButton(size: size, color: Colors.blue),
+                VerticalDivider(
+                  width: gap,
+                ),
+                CircleButton(size: size, color: Colors.green),
+              ]),
+          Container(
+            height: 2 * gap,
+          ),
+          const Text(
+            "Welcome to the color game!",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          Container(
+            height: gap,
+          ),
+          const Text(
+            "Simple Game, Try tap on the right color to proceed futher.",
+            textAlign: TextAlign.center,
+          )
+        ]);
   }
 }
 
 class CircleButton extends StatelessWidget {
-  final double? size;
-  final Color? color;
+  final double size;
+  final Color color;
 
-  const CircleButton({super.key, this.size, this.color});
+  const CircleButton({super.key, required this.size, required this.color});
 
   @override
   Widget build(BuildContext context) {
     return InkResponse(
         onTap: () {},
-        radius: size! / 2,
+        radius: size / 2,
         child: Container(
-            width:size,
+            width: size,
             height: size,
-            decoration:
-                BoxDecoration(color: color, shape: BoxShape.circle)));
+            decoration: BoxDecoration(color: color, shape: BoxShape.circle)));
   }
 }
